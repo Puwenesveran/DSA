f4a64b01ef640d808e8f4568c6a8ed3b628ca14d
Puwenesveran <128563001+Puwenesveran@users.noreply.github.com>
2023-04-01 21:07:34 +0530

    Came across a leetcode challenge and its hard but can be solved with making slight modifications in Binary Search

---
+++ Splitarray.java
@@ -0,0 +1,45 @@
+package Search;
+//https://leetcode.com/problems/split-array-largest-sum/
+public class Splitarray {
+    public static void main(String[] args) {
+        int[] nums={7,2,5,10,8};
+        int m=2;
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            start = Math.max(start, nums[i]); // in the end of the loop this will contain the max item of the array
+            end += nums[i];
+        }
+
+        // binary search
+        while (start < end) {
+            // try for the middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    // say you add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > m) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+        System.out.println(end); // here start == end
+
+
+    }
+}
